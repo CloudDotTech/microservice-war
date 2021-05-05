f5403f75eb321bcadb02139853b71a82f9230e33
First Last <email.mail.com>
2021-05-05 12:11:56 -0500

    first commit

---
+++ src/main/java/com/clouddottech/microservicewar/MicroserviceWarApplication.java
@@ -0,0 +1,27 @@
+package com.clouddottech.microservicewar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MicroserviceWarApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceWarApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MicroserviceWarApplication.class);
+    }
+
+    @RequestMapping(value = "/")
+    public String hello() {
+        return "Hello World from Tomcat!!!";
+    }
+}
---
+++ src/test/java/com/clouddottech/microservicewar/MicroserviceWarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clouddottech.microservicewar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceWarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

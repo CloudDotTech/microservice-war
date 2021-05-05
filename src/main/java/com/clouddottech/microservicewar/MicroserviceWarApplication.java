47d87efedc3b15f250a94bd5bce5ffdedd661f7f
First Last <email.mail.com>
2021-05-05 15:26:30 -0500

    first commit

--- src/main/java/com/clouddottech/microservicewar/MicroserviceWarApplication.java
+++ src/main/java/com/clouddottech/microservicewar/MicroserviceWarApplication.java
@@ -22,6 +22,6 @@ public class MicroserviceWarApplication extends SpringBootServletInitializer {
 
     @RequestMapping(value = "/")
     public String hello() {
-        return "Hello World from Tomcat!!!";
+        return "Hello World from Tomcat  *****";
     }
 }

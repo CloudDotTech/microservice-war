ce98ef13cb5bd263890dd3da38d4c6a782d7ad86
First Last <email.mail.com>
2021-05-09 01:39:30 -0500

    first commit 6

--- src/main/java/com/clouddottech/microservicewar/MicroserviceWarApplication.java
+++ src/main/java/com/clouddottech/microservicewar/MicroserviceWarApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @SpringBootApplication
 @RestController
 public class MicroserviceWarApplication extends SpringBootServletInitializer {
